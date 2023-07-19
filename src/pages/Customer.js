62d2bb2cd102e38443319e9e74768e6b9d6690cf
yeeydu <yeeysonduarte@gmail.com>
2023-07-19 18:34:43 +0100

    add dynamic update

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -7,6 +7,8 @@ import { baseUrl } from '../shared';
 export default function Customer() {
     const [customer, setCustomer] = useState([]);
     const [notFound, setNotFound] = useState(false);
+    const [tempCustomer, setTempCustomer] = useState([]);
+    const [changed, setChanged] = useState(false);
 
     const { id } = useParams() // use params returns an object {id} returns a paramter of object
     const navigate = useNavigate();
@@ -24,6 +26,7 @@ export default function Customer() {
                 return response.json()
             })
             .then((data) => {
+                setTempCustomer(data.customer)
                 setCustomer(data.customer) //.customer to get inside /property
             })
     }, [])
@@ -31,7 +34,7 @@ export default function Customer() {
     function deleteCustomer() {
         const url = baseUrl + 'api/customers/' + id;
         fetch(url, {
-            method: 'DELETE', 
+            method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json', // define content type in fetch
             }
@@ -47,13 +50,59 @@ export default function Customer() {
 
     }
 
+
+    function updateCustomer() {
+        const url = baseUrl + 'api/customers/' + id;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(tempCustomer),
+        })
+            .then((response) => {
+                return response.json
+            })
+            .then((data) => {
+                //setCustomer(data.customer)
+                
+                setChanged(false);
+            })
+            .catch((e) => {
+                console.log(e)
+            })
+    }
+
     return (
         <>
             {notFound ? <NotFound message={"Customer not found !"} /> : null}
             {customer ?
-                <div key={customer.id}>
-                    <h2>{customer.name}</h2>
-                    <p>{customer.industry}</p>
+                <div>
+                    <input
+                        className='mb-2 block px-2'
+                        type='text'
+                        value={tempCustomer.name}
+                        onChange={(e) => {
+                            setTempCustomer({ ...tempCustomer, name: e.target.value })
+                            setChanged(true)
+                        }} />
+                    <input
+                        className='mb-2 block px-2'
+                        type='text'
+                        value={tempCustomer.industry}
+                        onChange={(e) => {
+                            setTempCustomer({ ...tempCustomer, industry: e.target.value })
+                            setChanged(true)
+                        }} />
+                    {changed ? (
+                        <>
+                            <button onClick={updateCustomer}>Save</button>
+                            <button onClick={(e) => {
+                                setTempCustomer({ ...customer });
+                                setChanged(false)
+                            }}>Cancel</button>
+                        </>
+                    ) : null}
                 </div>
                 : null}
             <div>
