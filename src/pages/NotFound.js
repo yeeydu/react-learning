cda663c69dc3f9107f937c0a2dd70dc852266c4f
yeeydu <yeeysonduarte@gmail.com>
2023-08-02 18:25:53 +0100

    add back button navigation

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,16 +5,20 @@ import { Link, useNavigate } from 'react-router-dom'
 
 export default function NotFound({ message }) {
 
+  const navigate = useNavigate();
+  const goBack = () => {
+		navigate(-1);
+	}
 
   return (
     <>
       {message ?
         <div>{message}</div> : <p>Page not found</p>}
-      <Link to={'/dictionary'} >
-        <button
+ 
+        <button onClick={goBack}
           className=' m-2 px-4 py-1 text-sm font-semibold rounded-full border border-purple-200 text-white bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2 '
         >Return</button>
-      </Link>
+ 
     </>
 
   )
