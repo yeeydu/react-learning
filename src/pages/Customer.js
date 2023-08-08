101a84c0754971d84910e021a570acec8ef681ee
yeeydu <yeeysonduarte@gmail.com>
2023-08-08 18:46:00 +0100

    add undefined value on input value

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -121,11 +121,12 @@ export default function Customer() {
                                 id='name'
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 type='text'
-                                value={tempCustomer.name}
                                 onChange={(e) => {
                                     setTempCustomer({ ...tempCustomer, name: e.target.value })
                                     setChanged(true)
-                                }} />
+                                }}                               
+                                value={tempCustomer.name || ""}
+                                />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="industry" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Industry</label>
@@ -133,11 +134,12 @@ export default function Customer() {
                                 id='industry'
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 type='text'
-                                value={tempCustomer.industry}
                                 onChange={(e) => {
                                     setTempCustomer({ ...tempCustomer, industry: e.target.value })
                                     setChanged(true)
-                                }} />
+                                }} 
+                                value={tempCustomer.industry || ""}
+                                />
                         </div>
                     </form>
                     {changed ? (
